aa6dd520910655758b8eca32bb26eedc6ecc6fa1
ichiasa-g-3 <ichiasa-g-3@example.com>
2025-11-21 11:31:30 +0900

    EclipseGitTest_FirstCommit

---
+++ src/main/java/com/ikeda/EclipseGitTestSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ikeda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EclipseGitTestSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EclipseGitTestSpringProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ikeda/EclipseGitTestSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikeda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EclipseGitTestSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
