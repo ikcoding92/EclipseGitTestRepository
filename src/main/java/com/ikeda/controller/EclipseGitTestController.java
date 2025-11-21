b886115b8ef84381a4807eca26d41fbe9a621355
ichiasa-g-3 <ichiasa-g-3@example.com>
2025-11-21 13:32:23 +0900

    EditHTML

--- src/main/java/com/ikeda/controller/EclipseGitTestController.java
+++ src/main/java/com/ikeda/controller/EclipseGitTestController.java
@@ -1,14 +1,14 @@
 package com.ikeda.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class EclipseGitTestController {
 	
 	@GetMapping(value = "/sample")
 	public String method() {
-		return "Test Text Byebye(sekioka)";
+		return "dispTest";
 	}
 
 }
